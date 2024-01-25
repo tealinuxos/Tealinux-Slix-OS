c0334653439e6c9d96343ab47ee001144767d532
Khoirul Anam <akunnyakhoirulanam@gmail.com>
2024-01-25 13:43:29 +0700

    legi

--- tea-testing-baseline/profiledef.sh
+++ tea-testing-baseline/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-baseline"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux baseline"
+iso_name="tealinux"
+iso_label="TEA_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Tea Linux <https://tealinux.org>"
+iso_application="Tea Linux Testing"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -18,5 +18,10 @@ airootfs_image_type="squashfs"
 #airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
+	["/etc/shadow"]="0:0:0400"
+	["/etc/gshadow"]="0:0:0400"
+	["/etc/group"]="0:0:0400"
+
+	["/etc/skel/.config"]="1000:10:0755"
+	["/etc/skel/.zshrc"]="1000:10:0755"
 )
