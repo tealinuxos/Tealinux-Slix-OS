27274f85fffc0b7d93c48f386da9682a39237e5f
Khoirul Anam <akunnyakhoirulanam@gmail.com>
2024-01-25 10:57:25 +0700

    change baseline: erofs to squashfs

--- tea-testing-baseline/profiledef.sh
+++ tea-testing-baseline/profiledef.sh
@@ -13,8 +13,10 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
+#airootfs_image_type="erofs"
+airootfs_image_type="squashfs"
+#airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 )
