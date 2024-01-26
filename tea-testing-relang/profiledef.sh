894b8aadaefbea964f88de97603122b1a7cc2b03
Khoirul Anam <akunnyakhoirulanam@gmail.com>
2024-01-26 22:15:48 +0700

    to relang

--- tea-testing-relang/profiledef.sh
+++ tea-testing-relang/profiledef.sh
@@ -16,11 +16,14 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+	["/etc/shadow"]="0:0:400"
+	["/root"]="0:0:750"
+	["/root/.automated_script.sh"]="0:0:755"
+	["/root/.gnupg"]="0:0:700"
+	["/usr/local/bin/choose-mirror"]="0:0:755"
+	["/usr/local/bin/Installation_guide"]="0:0:755"
+	["/usr/local/bin/livecd-sound"]="0:0:755"
+
+  	["/etc/skel/.config"]="1000:10:0755"
+	["/etc/skel/.zshrc"]="1000:10:0755"
 )
