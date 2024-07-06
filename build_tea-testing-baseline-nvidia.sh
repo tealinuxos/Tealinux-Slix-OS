e3c3ea46cd1f62b6aebb0318746bce8c13a826a6
akunnyaanam <akunnyakhoirulanam@gmail.com>
2024-07-06 08:30:23 +0700

    done

---
+++ build_tea-testing-baseline-nvidia.sh
@@ -0,0 +1,2 @@
+sudo rm -rf .work_tea-testing-baseline-nvidia*
+sudo mkarchiso -vr -w .work_tea-testing-baseline-nvidia -o out_tea-testing-baseline-nvidia/ tea-testing-baseline-nvidia
--- build_tea-testing-baseline.sh
+++ build_tea-testing-baseline.sh
@@ -1,2 +1,2 @@
-sudo rm -rf .work*
+sudo rm -rf .work_tea-testing-baseline
 sudo mkarchiso -vr -w .work_tea-testing-baseline -o out_tea-testing-baseline/ tea-testing-baseline
